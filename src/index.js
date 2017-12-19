0b5acb47eddb655790fd9a092e5b66833193d194
dong.luo <dong.luo@dmall.com>
2017-12-19 19:58:48 +0800

    Production  5.Specify the Environment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { cube } from './math.js';
 
+if( process.env.NODE_ENV !== 'production'){
+   console.log( 'Look like we are in develpment mode!' );
+}
 function component() {
     var element = document.createElement('pre');
 
