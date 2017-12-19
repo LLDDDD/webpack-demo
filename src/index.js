9c931488ac98e37ca46fe6bab2f02268c200fc24
dong.luo <dong.luo@dmall.com>
2017-12-19 19:21:15 +0800

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
@@ -13,4 +14,14 @@ function component() {
 
     return element;
 }
+let element = component(); //Store the element to re-render on print.js changes
 document.body.appendChild(component());
+
+if(module.hot){
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component();// Re-render the 'component' to update the click handler
+    document.body.appendChild(element);
+  })
+}
